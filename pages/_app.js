6359a0729f1c8a81c130b191347c9f455ff3459d
Angelo Cruz <developer.angelo.cruz@gmail.com>
2022-12-21 20:36:02 +0000

    Google Tag Manager configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import Script from 'next/script'
 import { CacheProvider } from '@emotion/react'
 import { ThemeProvider, CssBaseline } from '@mui/material'
+import TagManager from 'react-gtm-module'
 
 import createEmotionCache from '../utility/createEmotionCache'
 import lightTheme from '../styles/theme/lightTheme'
@@ -11,13 +13,17 @@ const clientSideEmotionCache = createEmotionCache();
 const MyApp = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
 
+  React.useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-N3H9T76' })
+  }, [])
+
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
   )
 }
 
