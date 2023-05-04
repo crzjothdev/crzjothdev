fd99d65f30cb784a050dc1d5ffe2590b17951deb
Angelo Cruz <developer.angelo.cruz@gmail.com>
2023-05-04 14:43:39 +0100

    Showing up the projects section

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -2,6 +2,8 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
 
+import ProjectCard from '../ProjectCard'
+
 export default function Projects() {
     return (
         <Box id="projects">
@@ -21,6 +23,14 @@ export default function Projects() {
                 }
             })}>
                 <Grid container spacing={4}>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <ProjectCard
+                            title="Professional Profile"
+                            subtitle="Built with NextJS 13"
+                            thumbnail="/images/logo.webp"
+                            gitHubUrl="https://github.com/crzjothdev"
+                        />
+                    </Grid>
                 </Grid>
             </Box>
         </Box>
